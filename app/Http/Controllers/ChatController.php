a3b759968dfca6b44f6ffc06ed833a52a48cbadc
BerkeG <runjeberke@gmail.com>
2021-08-16 10:52:39 +0300

    added controllers & models

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request)
+    {
+        return ChatRoom::all();
+    }
+    public function messages(Request $request, $roomid)
+    {
+        return
+            ChatMessage::where('room_id', $roomid)
+            ->with('user')
+            ->orderBy('create_at', 'DESC')
+            ->get();
+    }
+    public function newMessage(Request $request,$roomid)
+    {
+        $newMessage=new ChatMessage;
+        $newMessage->user_id=Auth::id();
+        $newMessage->room_id=$roomid;
+        $newMessage->message=$request->message;
+        $newMessage->save();
+
+        return $newMessage;
+    }
+}
